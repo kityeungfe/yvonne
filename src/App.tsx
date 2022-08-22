8622c87170aa1ee8864922d4957d9f0e5bb75520
kityeungfe <kityeungfe@outlook.com>
2022-08-23 04:41:44 +0800

    add flowbite and tooltip

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@
  * @Author: kityeungfe kityeungfe@outlook.com
  * @Date: 2022-08-23 03:23:44
  * @LastEditors: kityeungfe kityeungfe@outlook.com
- * @LastEditTime: 2022-08-23 04:09:17
+ * @LastEditTime: 2022-08-23 04:40:43
  * @FilePath: \yvonne\src\App.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+import { Tooltip } from 'flowbite-react'
 import { Fragment, ReactElement } from 'react'
 import { Card } from './components/Card'
 import { CardContent } from './components/Card/CardContent'
@@ -25,11 +26,43 @@ const App : React.FC<IProp> = ():ReactElement => {
         <CardContent>
           <div className="text-xl font-medium text-center">Yvonne Cheung</div>
           <p className="text-slate-500 my-2 text-center rounded-full bg-[#F0F0F5] px-4">Foreign trade clerk</p>
-          <div className='my-6 flex flex-row space-x-6 justify-center'>
-            <img className="h-4 w-4" src={'./icon_whatsapp.png'} alt="whatsapp icon" />
-            <img className="h-4 w-4" src={'./icon_wechat.png'} alt="wechat icon" />
-            <img className="h-4 w-4" src={'./icon_facebook.png'} alt="facebook icon" />
-            <img className="h-4 w-4" src={'./icon_instream.png'} alt="instream icon" />
+          <div className='my-6 flex flex-row justify-center'>
+            <Tooltip 
+              style="light"
+              content={(
+                <Fragment>
+                  <div className="text-xl font-medium w-full">About Me</div>
+                  <div className="w-14 h-4 text-slate-500 my-2 rounded-full bg-[#2422c2] px-4"></div>
+                  <div className='my-6 flex flex-row space-x-6 justify-center'>
+                    <img className="h-4 w-4" src={'./icon_whatsapp.png'} alt="whatsapp icon" />
+                    <img className="h-4 w-4" src={'./icon_wechat.png'} alt="wechat icon" />
+                    <img className="h-4 w-4" src={'./icon_facebook.png'} alt="facebook icon" />
+                    <img className="h-4 w-4" src={'./icon_instream.png'} alt="instream icon" />
+                  </div>
+                </Fragment>
+              )}
+            >
+              <img className="h-4 w-4 mx-4" src={'./icon_whatsapp.png'} alt="whatsapp icon" />
+            </Tooltip>
+            <Tooltip 
+              style="light"
+              content={(
+                <Fragment>
+                  <div className="text-xl font-medium w-full">About Me</div>
+                  <div className="w-14 h-4 text-slate-500 my-2 rounded-full bg-[#2422c2] px-4"></div>
+                  <div className='my-6 flex flex-row space-x-6 justify-center'>
+                    <img className="h-4 w-4" src={'./icon_whatsapp.png'} alt="whatsapp icon" />
+                    <img className="h-4 w-4" src={'./icon_wechat.png'} alt="wechat icon" />
+                    <img className="h-4 w-4" src={'./icon_facebook.png'} alt="facebook icon" />
+                    <img className="h-4 w-4" src={'./icon_instream.png'} alt="instream icon" />
+                  </div>
+                </Fragment>
+              )}
+            >
+              <img className="h-4 w-4 mx-4" src={'./icon_wechat.png'} alt="wechat icon" />
+            </Tooltip>
+            <img className="h-4 w-4 mx-4" src={'./icon_facebook.png'} alt="facebook icon" />
+            <img className="h-4 w-4 mx-4" src={'./icon_instream.png'} alt="instream icon" />
           </div>
         </CardContent>
         <CardFooter>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,22 @@
+/*
+ * @Author: kityeungfe kityeungfe@outlook.com
+ * @Date: 2022-08-23 03:23:44
+ * @LastEditors: kityeungfe kityeungfe@outlook.com
+ * @LastEditTime: 2022-08-23 04:18:28
+ * @FilePath: \yvonne\tailwind.config.cjs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 /** @type {import('tailwindcss').Config} */ 
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
