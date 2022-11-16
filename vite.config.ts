d03fbcf4459b013ebeaac28619fd67442a0f4d8e
kityeungfe <kityeungfe@outlook.com>
2022-11-17 02:09:38 +0800

    add carousel

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/docs': {
+        target: 'https://flowbite.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/docs/, '/docs') // 不可以省略rewrite
+      }
+    }
+  }
 })
