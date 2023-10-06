c2893e2299009aed6614c5247fa83fb3a2db49b1
Kit Yeung <kit.yeung@cs.hkbn.com.hk>
2023-10-06 15:34:14 +0800

    feat: test

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA()
+  ],
+  base: "yvonne",
+  build: {
+    outDir: "build"
+  },
   server: {
     port: 3000,
     proxy: {
